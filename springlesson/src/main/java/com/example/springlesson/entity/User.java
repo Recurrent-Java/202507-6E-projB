a102e10839484602cb4d4ab9cf9be8312c77ff76
10-fujita <fujitahironari1019@gmail.com>
2025-12-15 16:23:20 +0900

    userエンティティを変更https://github.com/Recurrent-Java/202507-6E-projB.git into main

--- springlesson/src/main/java/com/example/springlesson/entity/User.java
+++ springlesson/src/main/java/com/example/springlesson/entity/User.java
@@ -1,5 +1,7 @@
 package com.example.springlesson.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,19 +10,46 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity  // DBからのデータ格納用クラスであることを表す
-@Table(name="customer")
+@Table(name="users")
 @Data  // getter/setterを自動で作成してくれる
+@NoArgsConstructor  // 引数なしのコンストラクターをlombokが作成してくれる
 public class User {
   @Id  // 主キーを表す
   @GeneratedValue(strategy = GenerationType.IDENTITY)  // AUTO_INCREMENTの場合付与が必要
   @Column(name="id")  // DB上のカラム名と変数名を別にしたい場合は必須
   private Integer id;
   
-  @Column(name="login")  // DB上のカラム名と変数名を別にしたい場合は必須
-  private String login;
+  @Column(name="email", nullable =false)  // DB上のカラム名と変数名を別にしたい場合は必須
+  private String email;
   
-  @Column(name="password")  // DB上のカラム名と変数名を別にしたい場合は必須
+  @Column(name="password_hash", nullable =false)  // DB上のカラム名と変数名を別にしたい場合は必須
   private String password;
+  
+  @Column(name="phone_number")  // DB上のカラム名と変数名を別にしたい場合は必須
+  private String phoneNumber;
+  
+  @Column(name="dm_opt_in", nullable =false)  // DB上のカラム名と変数名を別にしたい場合は必須
+  private boolean dmOptIn; ;
+  
+  @Column(name="withdraw_flag", nullable =false)  // DB上のカラム名と変数名を別にしたい場合は必須
+  private boolean withdrawFlag;
+  
+  @Column(name="last_login_at")  // DB上のカラム名と変数名を別にしたい場合は必須
+  private LocalDateTime lastLoginAt;
+  
+  @Column(name = "created_at", insertable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", insertable = false, updatable = false)
+  private LocalDateTime updatedAt;
+
+  // createdAt, updatedAt はDB側で自動設定するため、アプリケーション側では設定しない
+
+  // lastLoginAt はログイン時に別で更新する
+  public void updateLastLogin() {
+      this.lastLoginAt = LocalDateTime.now();
+  }
 }
