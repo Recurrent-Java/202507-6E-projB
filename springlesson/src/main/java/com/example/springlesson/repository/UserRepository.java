7b34579747df5c0ed3585b18d7d4267b231d9976
Seki <gshunta1216@gmail.com>
2025-12-16 15:19:22 +0900

    UserRepositoryの編集。

--- springlesson/src/main/java/com/example/springlesson/repository/UserRepository.java
+++ springlesson/src/main/java/com/example/springlesson/repository/UserRepository.java
@@ -7,4 +7,6 @@ import com.example.springlesson.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 public User findByLogin(String login);
 public User save(User user);
+// ★ メールアドレス検索（追加）
+User findByEmail(String email);
 }
