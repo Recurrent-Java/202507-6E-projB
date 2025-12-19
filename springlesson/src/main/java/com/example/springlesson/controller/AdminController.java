2a0f503547b9963e53f5362d610c622b9721fb5d
10-fujita <fujitahironari1019@gmail.com>
2025-12-19 17:03:11 +0900

    管理者画面周り

--- springlesson/src/main/java/com/example/springlesson/controller/AdminController.java
+++ springlesson/src/main/java/com/example/springlesson/controller/AdminController.java
@@ -4,15 +4,18 @@ import java.security.Principal;
 import java.util.List;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.springlesson.entity.User;
+import com.example.springlesson.form.AdminCustomerForm;
 import com.example.springlesson.repository.UserRepository;
 import com.example.springlesson.service.AdminService;
 
@@ -55,9 +58,16 @@ public class AdminController {
   }
 
   @PostMapping("/checkUser")
-  public String checkUser(HttpSession session, @RequestParam("userId") List<Long> userIds, Model model) {
+  public String checkUser( @Valid @ModelAttribute("AdminCustomerForm") AdminCustomerForm form,
+      BindingResult bindingResult,
+      HttpSession session,
+      Model model) {
+    if (bindingResult.hasErrors()) {
+      //エラー時の処理
+      return "admin/userManagement/";
+    }
     try {
-      List<User> userList = adminService.findByIdIn(userIds);
+      List<User> userList = adminService.findByIdIn(form.getUserIds());
       session.setAttribute("userCheckList", userList);
       model.addAttribute("userList", userList);
       return "admin/adminUserCheck";
---
+++ springlesson/src/main/java/com/example/springlesson/form/AdminCustomerForm.java
@@ -0,0 +1,23 @@
+package com.example.springlesson.form;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class AdminCustomerForm {
+
+ @NotNull(message = "ユーザーが選択されていません。")
+ private List<Long> userIds;
+
+  public List<Long> getUserIds() {
+    return userIds;
+  }
+  public void setUserId(List<Long> userIds) {
+    this.userIds = userIds;
+ }
+ 
+ 
+}
