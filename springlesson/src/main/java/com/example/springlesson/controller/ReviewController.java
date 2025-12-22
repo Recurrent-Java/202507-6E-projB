6f4d49e06ebfb9cfbc89439198bd94e6b758a43f
Yamasaki <yamasaki.a723@gmail.com>
2025-12-22 12:52:51 +0900

    Merge branch 'main' of https://github.com/Recurrent-Java/202507-6E-projB.git into main

--- springlesson/src/main/java/com/example/springlesson/controller/ReviewController.java
+++ springlesson/src/main/java/com/example/springlesson/controller/ReviewController.java
@@ -1,12 +1,23 @@
 package com.example.springlesson.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/review")
 public class ReviewController {
+  // レビュー一覧ページ
+  @GetMapping
+  public String list() {
+    return "review/reviewList";
+  }
   
+  // レビュー投稿ページ
+  @GetMapping("/new")
+  public String newReview() {
+    return "review/reviewForm";
+  }
   
   
 }
