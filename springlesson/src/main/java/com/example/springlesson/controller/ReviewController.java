0ce8a9d9a45abb39c750c7131468de1484700714
10-fujita <fujitahironari1019@gmail.com>
2025-12-25 16:34:30 +0900

    レビューの表示

--- springlesson/src/main/java/com/example/springlesson/controller/ReviewController.java
+++ springlesson/src/main/java/com/example/springlesson/controller/ReviewController.java
@@ -1,15 +1,30 @@
 package com.example.springlesson.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.springlesson.service.ReviewService;
 
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
+  private final ReviewService reviewService;
+  
+  public ReviewController(ReviewService reviewService) {
+    this.reviewService = reviewService;
+  }
   // レビュー一覧ページ
   @GetMapping
-  public String list() {
+  public String list(Model model) {
+    try {
+      reviewService.getLatest5Reviews();
+      
+    }catch (Exception e) {
+      e.printStackTrace();
+      model.addAttribute("errMsg", "レビュー情報の取得に失敗しました。");
+    }
     return "review/reviewList";
   }
   
