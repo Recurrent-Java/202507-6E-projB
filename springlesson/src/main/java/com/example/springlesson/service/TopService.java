042519d8cb487e7e36856b5894a085759490d288
10-fujita <fujitahironari1019@gmail.com>
2025-12-25 17:08:53 +0900

    レビュー表示に関する処理

--- springlesson/src/main/java/com/example/springlesson/service/TopService.java
+++ springlesson/src/main/java/com/example/springlesson/service/TopService.java
@@ -3,7 +3,6 @@ package com.example.springlesson.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.springlesson.entity.Review;
 import com.example.springlesson.repository.ReviewRepository;
@@ -15,7 +14,7 @@ private final ReviewRepository reviewRepository;
 public TopService(ReviewRepository reviewRepository) {
   this.reviewRepository = reviewRepository;
 }
-@Transactional
+
 public List<Review> getTop2Reviews() {
     return reviewRepository.findTop2ByOrderByIdAsc();
 
